b99cfa2c50d8b62c0e4bc01f658b89b953af86ea
yyhslyz <164326235@qq.com>
2020-09-07 00:06:17 +0800

    Revert "show news (naive implementation)"
    This reverts commit c6d80aa4

--- app/src/main/java/com/java/luoyizhen/NewsList.java
+++ app/src/main/java/com/java/luoyizhen/NewsList.java
@@ -14,7 +14,7 @@ import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 
-import org.asynchttpclient.*;
+//import org.asynchttpclient.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
