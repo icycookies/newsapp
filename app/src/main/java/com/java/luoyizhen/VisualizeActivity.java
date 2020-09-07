7c74d2e95be1afa217c2a5cffc7c5f35a9891577
yyhslyz <164326235@qq.com>
2020-09-07 19:39:12 +0800

    add visualization

--- app/src/main/java/com/java/luoyizhen/VisualizeActivity.java
+++ app/src/main/java/com/java/luoyizhen/VisualizeActivity.java
@@ -7,9 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class VisualizeActivity extends AppCompatActivity {
 
@@ -54,6 +66,30 @@ public class VisualizeActivity extends AppCompatActivity {
         text1.setText(Integer.toString(data.getCurCount()));
         text2.setText(Integer.toString(data.getDeadCount()));
         BarChart barChart = findViewById(R.id.rank);
-
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        final ArrayList<Map.Entry<String, Integer>> chartData = curField == 0 ? data.getProvince() : data.getCountry();
+        int i = 0;
+        for (Map.Entry<String, Integer> elem : chartData){
+            barEntries.add(new BarEntry((float) i, elem.getValue().floatValue()));
+        }
+        BarDataSet barDataSet = new BarDataSet(barEntries,"");
+        BarData data = new BarData(barDataSet);
+        barChart.setData(data);
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return chartData.get((int)value).getKey();
+            }
+        });
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getAxisRight().setEnabled(false);
+        barChart.animateY(1000, Easing.EaseInBack);
+        barChart.animateX(1000, Easing.EaseInBack);
+        Description desc = new Description();
+        desc.setText(curField == 0 ? "全国各省份数据统计" : "全球各国家数据统计");
+        barChart.setDescription(desc);
     }
 }
